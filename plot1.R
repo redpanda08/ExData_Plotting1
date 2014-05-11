dd6a480843e8087059a6ba6e3372e3a9b4222cfc
minyubian <joyce.m.bian@gmail.com>
2014-05-11 20:29:45 +0100

    courseproject1

--- plot1.R
+++ plot1.R
@@ -20,7 +20,7 @@ test2 <- test[test$Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02")),]
 ##plot the histogram for Global_active_power variable
 attach(test2)
 
-png(file="plotmy.png",bg="transparent")
+png(file="plot1.png",bg="transparent")
 hist(Global_active_power, main="Global Active Power", col="red",
      xlab="Global Active Power (kilowatts)")
 dev.off()
